045d6ce9a118f20d1669524cadf79788c4863ff7
Shiqi Tong <shiqi.tong@shiqis-MacBook-Pro.local>
2021-01-20 10:20:05 +0800

    completed

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,16 @@
-const parseData
+const parseData = (input) => {
+    let {data: records, column: columns} = input
+
+    let result = [];
+    
+    for(let record of records) {
+        let json = {};
+        for(let i in columns) {
+            json[columns[i].name] = record[i];
+        }
+        result.push(json)
+    }
+    return result;
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,23 @@
-const inject
+const combineArrays = (array1, array2, start, end) => {
+    for(let i = start; i < end; i+=1) {
+        array1.push(array2[i])
+    }
+    return array1;
+}
+
+const inject = (items, sections) => {
+    
+    let result = [];
+    let i = 0;
+    for(let section of sections) {
+        let {content, index} = section;
+        combineArrays(result, items, i, index);
+        result.push(content);
+        i = index;
+    }
+    combineArrays(result, items, i, items.length);
+
+    return result;
+}
+
 export { inject };
